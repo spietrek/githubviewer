9c539000f710d74df415eb129eac85ee6aa46c83
Steve Pietrek <spietrek@gmail.com>
2016-03-14 14:55:34 -0400

    Add ability to search for users.

--- index.ios.js
+++ index.ios.js
@@ -4,19 +4,28 @@ import React, {
   AppRegistry, Component, StyleSheet, NavigatorIOS, View, StatusBarIOS
 } from 'react-native';
 import Main from './App/Components/Main';
+import Search from './App/Components/Search';
 
 class GitHub extends Component {
   render() {
     /* beautify ignore:start */
     const initialRoute = {
       title: 'GitHub Viewer',
-      component: Main
+      component: Main,
+      rightButtonIcon: require('./App/Images/search-20.png'),
+      onRightButtonPress: () => {
+        this.refs.nav.push({
+          title: 'Search Users',
+          component: Search
+        });
+      }
     };
     
     StatusBarIOS.setStyle('light-content');
 
     return (
       <NavigatorIOS
+        ref='nav'
         barTintColor='#333333'
         tintColor='#48BBEC'
         titleTextColor='#FFF'
